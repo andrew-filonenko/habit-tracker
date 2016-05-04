8513c39e48ed6bd6ff28b21c898f39d96a9a8259
Andrei Filonenko <afilonenko@guidewire.com>
2016-05-04 05:13:03 +0100

    Add actions for user management

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -2,11 +2,10 @@ import db from '../db';
 import { createAction } from 'redux-actions';
 
 export const USER_LOGIN = 'USER_LOGIN';
-export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
 export const USER_SIGNUP = 'USER_SIGNUP';
-export const USER_SIGNUP_SUCCESS = 'USER_SIGNUP_SUCCESS';
-export const USER_SIGNUP_FAIL = 'USER_SIGNUP_FAIL';
 export const USER_LOGOUT = 'USER_LOGOUT';
 
 export const login = createAction(USER_LOGIN, (username, password) => db.login(username, password));
+export const signup = createAction(USER_SIGNUP, (username, password) => db.signup(username, password));
+export const logout = createAction(USER_LOGOUT, () => db.logout());
 
